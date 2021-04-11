48c2cd6d89c5b28d4870d3efa8c22c5e0bad6678
Alexandre Salomé <git@pub.salome.fr>
2021-04-11 18:10:09 +0200

    Rework header

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,10 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
-;
+
+  .copyFiles({
+      from: './assets/images',
+      to: 'images/[path][name].[hash:8].[ext]'
+  })
 
 module.exports = Encore.getWebpackConfig();
